89f952276d6d3203ac405d3ea0b4d52f3b4068da
Akinola <akinolatofunmi04@gmail.com>
2023-03-28 21:40:33 +0000

    Task 0 - Lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+"""
+Implement lockboxes
+"""
+
+
+def canUnlockAll(boxes):
+
+    """
+    Takes a list of lists as an argument and where each box contains the key
+    to the any other box, the first box is unloack by default
+    """
+
+    obj = {}
+    for box in range(len(boxes)):
+        # print(obj)
+        # print('Box ', box)
+        if box > 0 and box not in obj.keys():
+            for key, value in obj.items():
+                # if key == box and value == 'unlocked':
+                if box in boxes[key]:
+                    return True
+            return False
+        keys = boxes[box]
+        for key in range(len(keys)):
+            if keys[key] < len(boxes):
+                element = keys[key]
+                obj[element] = 'unlocked'
+            else:
+                obj[element] = 'locked'
+    return True
