ed7b32cae998ba84f83e105cf3778b516d1d3492
Akinola <akinolatofunmi04@gmail.com>
2023-03-30 20:18:03 +0000

    Use enumerate instead of range && document

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -12,30 +12,25 @@ def canUnlockAll(boxes):
     """
 
     obj = {}
-    if len(boxes) <= 0:
-        return False
-    for box in range(len(boxes)):
+    for box, keys in enumerate(boxes):
         can_unlock = False
-        # print('Box ', box)
-        if box > 0 and box not in obj.keys():
-            for key, value in obj.items():
-                # print(box)
-                # if key == box and value == 'unlocked':
+        if box > 0 and box not in obj:
+            # if box is not already unlocked we want to check if an
+            # an already unlocked box has the key to unlock that box
+            for key in obj:
                 if key < len(boxes) and box in boxes[key]:
-                    # print('here')
                     can_unlock = True
                     break
             if not can_unlock:
                 return False
             continue
         keys = boxes[box]
-        for key in range(len(keys)):
-            element = keys[key]
-            if keys[key] < len(boxes):
+        for key in keys:
+            # element = key
+            if key < len(boxes):
                 # checks if key is a valid box index
                 # add the key as the 'key' to the object, obj
-                # element = keys[key]
-                obj[element] = 'unlocked'
+                obj[key] = 'unlocked'
             else:
-                obj[element] = 'locked'
+                obj[key] = 'locked'
     return True
