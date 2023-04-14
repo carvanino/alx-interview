63af5205dad740b5589075c07a3d49c4eca2875c
Akinola <akinolatofunmi04@gmail.com>
2023-04-14 00:37:16 +0000

    Add function to organize

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -3,56 +3,43 @@
 Task 0 - Log parsing
 """
 
-# from sys import stdin
 import sys
 
-count = 0
-fileSize = 0
-pStatusCode = [200, 301, 400, 401, 403, 404, 405, 500]
-statusCodeOccurence = {}
 
-try:
-    for line in sys.stdin:
-        try:
-            each = line.split()
-            count = count + 1
-            # print(each[-1:][0])
-            # fileSize += int(each[len(each)-1])
-            fileSize += int(each[-1:][0])
-            statusCode = int(each[-2:][0])
-            codeCount = 0
-            if statusCode in pStatusCode:
-                if statusCode in statusCodeOccurence:
-                    statusCodeOccurence[statusCode] += 1
-                else:
-                    statusCodeOccurence[statusCode] = 1
+def parse_log():
+    count = 0
+    fileSize = 0
+    pStatusCode = [200, 301, 400, 401, 403, 404, 405, 500]
+    statusCodeOccurence = {}
 
-            # print("line {} -> {}".format(count, each))
+    try:
+        for line in sys.stdin:
+            try:
+                each = line.split()
+                count += 1
+                fileSize += int(each[-1:][0])
+                statusCode = int(each[-2:][0])
+                if statusCode in pStatusCode:
+                    if statusCode in statusCodeOccurence:
+                        statusCodeOccurence[statusCode] += 1
+                    else:
+                        statusCodeOccurence[statusCode] = 1
 
-            if count == 10:
-                count = 0
-                raise ValueError("Count Reached 10")
-                '''
-                print('File size: {}'.format(fileSize))
-                # print(statusCodeOccurence)
-                for items in sorted(statusCodeOccurence.items()):
-                    print('{}: {}'.format(items[0], items[1]))
-                    # print(list(items))
-                statusCodeOccurence.clear()
-                '''
-                # print
-                # raise ValueError("Count Reached 10")
-        except ValueError as err:
-            print('File size: {}'.format(fileSize))
-            for items in sorted(statusCodeOccurence.items()):
-                print('{}: {}'.format(items[0], items[1]))
-            # statusCodeOccurence.clear()
-            # print("Exception occurred:", err)
-            # print(count)
-            continue
-except KeyboardInterrupt:
+                if count == 10:
+                    count = 0
+                    raise ValueError("Count Reached 10")
+            except ValueError as err:
+                print_error(fileSize, statusCodeOccurence, err)
+                continue
+    except KeyboardInterrupt:
+        print_error(fileSize, statusCodeOccurence, "KeyboardInterrupt")
+
+
+def print_error(fileSize, statusCodeOccurence, error):
     print('File size: {}'.format(fileSize))
     for items in sorted(statusCodeOccurence.items()):
         print('{}: {}'.format(items[0], items[1]))
-    # statusCodeOccurence.clear()
-    # print("KeyboardInterrupt occurred")
+
+
+if __name__ == '__main__':
+    parse_log()
