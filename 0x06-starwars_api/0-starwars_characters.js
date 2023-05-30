86182e156ab8259727ed51d2a387744757fbe057
Akinola <akinolatofunmi04@gmail.com>
2023-05-30 07:45:50 +0000

    Solving with async and await

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -5,14 +5,21 @@ const argv = process.argv;
 
 const api = 'https://swapi-api.alx-tools.com/api/films/';
 
-request.get(`${api}${argv[2]}`, function (error, response, body) {
+async request.get(`${api}${argv[2]}`, function (error, response, body) {
   if (!error) {
     const data = JSON.parse(body);
     const characters = data.characters;
-    printCharName(characters, 0);
+    for (const character of characters) {
+      await request.get(character, function(error, response, body) {
+        if (!error) {
+	  console.log(JSON.parse(body).name);
+	}
+      }
+    }
   }
 });
 
+/*
 function printCharName (characters, index) {
   request.get(characters[index], function (error, response, body) {
     if (!error) {
@@ -25,3 +32,4 @@ function printCharName (characters, index) {
     }
   });
 }
+*/
