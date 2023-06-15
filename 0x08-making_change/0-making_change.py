a4816b196e5e82d75e7c14a652a601f64be0a0de
Akinola <akinolatofunmi04@gmail.com>
2023-06-15 22:58:47 +0000

    Remove redundancy

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -11,6 +11,7 @@ def makeChange(coins, total):
     coins = sorted(coins)
     if total <= 0:
         return 0
+    '''
     maxi = max(coins)
     hmax = 0
     count = 0
@@ -20,9 +21,11 @@ def makeChange(coins, total):
 
     target = total - hmax
     coins.remove(maxi)
+    '''
+    count = 1
     finder = 0
     maxi = max(coins)
-    # count = 1
+    target = total - maxi
     while (finder != target):
         if (finder < target):
             finder += maxi
@@ -37,7 +40,6 @@ def makeChange(coins, total):
         if finder == target:
             # coins.remove(maxi)
             return count
-
     '''
     print('Finder is ', finder)
     print('Count is ', count)
@@ -53,6 +55,7 @@ if __name__ == "__main__":
     print(makeChange([1, 5, 10, 25], 43))
 
     print(makeChange([1, 7, 13, 21], 50))
+
 
     print(makeChange([1, 3, 5, 9], 15))
 
